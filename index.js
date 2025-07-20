1bf13fe52816962df27cdb0ca900d48e3b1edc8b
mingmungXD <rex02033@gmail.com>
2025-07-20 20:37:05 +0900

    cors 설정

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const cors = require('cors');
 
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ['https://tripon-fe.vercel.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 app.use(express.json());
 
 const analyzeRoutes = require('./server/routes/analyzeRoutes');
